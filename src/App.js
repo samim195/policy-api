41cce49ba9883ba1a94fb34f8d8ffb19b3b12e59
samimkohistani <samimkohistani@hotmail.com>
2021-01-13 20:25:32 +0000

    first commit, project complete, styling in App.ss, Axios http request, react-router, react-hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,119 @@
-import logo from './logo.svg';
+/* eslint-disable no-useless-concat */
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
+import { Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-function App() {
+const App = () => {
+  const [policy_ref, updatePolicy] = useState('');
+  const [coverType, updateCover] = useState('');
+  const [car, updateCar] = useState('');
+  const [address, updateAddress] = useState('');
+  const [hide, updateHide] = useState(true);
+
+  function Login() {
+    const username = document.getElementById('InputUsername').value
+    console.log(username);
+    const password = document.getElementById('InputPassword').value
+    console.log(password);
+  
+    const url = 'https://api.bybits.co.uk/auth/token'
+    const headers = {'environment': 'mock', 'Content-Type': 'application/json'}
+    const data = {
+      "username":"testuser",
+      "password":"EbpucVzUP5cvsYha0E9i",
+      "type":"USER_PASSWORD_AUTH"
+    }
+    axios.post(url, data, {headers:headers})
+    .then((resp) => {
+      console.log(resp)
+      var accessToken = resp.data.access_token
+      console.log(accessToken)
+  
+      const auth = 'Bearer ' + accessToken
+      const urlTwo = 'https://api.bybits.co.uk/policys/details';
+      const headerTwo = {'environment': 'mock', Authorization: auth, 'Content-Type': 'application/json'}
+      axios.get(urlTwo, {headers:headerTwo})
+      .then((response) => {
+        console.log(response)
+        const result = response.data;
+        const policyRef = result.policy.policy_ref;
+        const coverType = result.policy.cover;
+        console.log(policyRef)
+        console.log(coverType)
+        const car = result.vehicle.make + ' ' + result.vehicle.model + ' ' + result.vehicle.colour + 
+        ' ' + '-' + result.vehicle.reg;
+        console.log(car)
+        const address = result.policy.address.line_1 + ', ' + result.policy.address.line_2 + ', ' +
+        result.policy.address.postcode
+        console.log(address);
+        // const insertHtml = "<div"
+        updatePolicy(policyRef)
+        updateCover(coverType)
+        updateCar(car)
+        updateAddress(address)
+        updateHide(!hide)
+      });
+    });
+    
+  
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+    <Route exact path={['/home', '/']}>
+    <div className="">
+      <header className="header">
       </header>
-    </div>
+      <form className='header' style={{display: hide ? 'block' : 'none'}}>
+      <h1>Sign In</h1>
+        <div className='form-group'>
+          <label>User Name</label>
+          <input type="text" className="form-control" id="InputUsername" placeholder="Enter Username"/>
+        </div>
+        <div className='form-group'>
+          <label>Password</label>
+          <input type="password" className="form-control" id="InputPassword" placeholder='Password'></input>
+        </div>
+        <div className="">
+        <Link
+        to={{
+            pathname: "/policy",
+        }}
+        >
+        <button type="submit" className="btn btn-primary" onClick={Login}>Sign In</button>
+        </Link>
+        </div>
+      </form>
+      </div>
+      </Route>
+      <Route path='/policy'>
+      <div id='output' style={{display: hide ? 'none' : 'block'}} className='resultsBox'>
+      <div>
+        <h2>My Policy</h2>
+        <div>
+          <h4>Policy reference:</h4>
+          <p>{policy_ref}</p>
+        </div>
+        <div>
+          <h4>Cover Type:</h4>
+          <p>{coverType}</p>
+        </div>
+        <div>
+          <h4>Car:</h4>
+          <p>{car}</p>
+        </div>
+        <div>
+          <h4>Address:</h4>
+          <p>{address}</p>
+        </div>
+
+
+      </div>
+      </div>
+    </Route>
+    </Router>
   );
 }
 
