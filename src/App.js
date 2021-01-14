301c67b79b1b0fb81fae6b1f5db8b3e7ec66dc45
samimkohistani <samimkohistani@hotmail.com>
2021-01-14 20:53:30 +0000

    parsing values to axios, using react hooks to capture input

--- src/App.js
+++ src/App.js
@@ -6,32 +6,32 @@ import { Route, Link } from 'react-router-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 const App = () => {
+  const [username, updateUser] = useState('')
+  const [password, updatePassword] = useState('')
   const [dataArray, updateData] = useState({policy_ref: '', coverType: '',
                    car: '', address: ''})
 
   function Login() {
-    const username = document.getElementById('InputUsername').value
     console.log(username);
-    const password = document.getElementById('InputPassword').value
     console.log(password);
   
-    const url = 'https://api.bybits.co.uk/auth/token'
-    const headers = {'environment': 'mock', 'Content-Type': 'application/json'}
+    const urlLogin = 'https://api.bybits.co.uk/auth/token'
+    const headerLogin = {'environment': 'mock', 'Content-Type': 'application/json'}
     const data = {
-      "username":"testuser",
-      "password":"EbpucVzUP5cvsYha0E9i",
+      "username":username,
+      "password":password,
       "type":"USER_PASSWORD_AUTH"
     }
-    axios.post(url, data, {headers:headers})
+    axios.post(urlLogin, data, {headers:headerLogin})
     .then((resp) => {
       console.log(resp)
       var accessToken = resp.data.access_token
       console.log(accessToken)
   
       const auth = 'Bearer ' + accessToken
-      const urlTwo = 'https://api.bybits.co.uk/policys/details';
-      const headerTwo = {'environment': 'mock', Authorization: auth, 'Content-Type': 'application/json'}
-      axios.get(urlTwo, {headers:headerTwo})
+      const urlPolicy = 'https://api.bybits.co.uk/policys/details';
+      const headerPolicy = {'environment': 'mock', Authorization: auth, 'Content-Type': 'application/json'}
+      axios.get(urlPolicy, {headers:headerPolicy})
       .then((response) => {
         console.log(response)
         const result = response.data;
@@ -64,11 +64,11 @@ const App = () => {
       <h1>Sign In</h1>
         <div className='form-group'>
           <label>User Name</label>
-          <input type="text" className="form-control" id="InputUsername" placeholder="Enter Username"/>
+          <input type="text" onChange={e => updateUser(e.target.value)} className="form-control" id="InputUsername" placeholder="Enter Username"/>
         </div>
         <div className='form-group'>
           <label>Password</label>
-          <input type="password" className="form-control" id="InputPassword" placeholder='Password'></input>
+          <input type="password" onChange={e => updatePassword(e.target.value)} className="form-control" id="InputPassword" placeholder='Password'></input>
         </div>
         <div className="">
         <Link
